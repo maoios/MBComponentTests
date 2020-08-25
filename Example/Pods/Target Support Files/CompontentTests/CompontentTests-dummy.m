188457f781e58d18c0da392c6f9778e09be01825
1246970327@qq.com <1246970327@qq.com>
2020-08-25 11:31:52 +0800

    init library

---
+++ Example/Pods/Target Support Files/CompontentTests/CompontentTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CompontentTests : NSObject
+@end
+@implementation PodsDummy_CompontentTests
+@end
---
+++ Example/Pods/Target Support Files/Pods-CompontentTests_Tests/Pods-CompontentTests_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CompontentTests_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CompontentTests_Tests
+@end
